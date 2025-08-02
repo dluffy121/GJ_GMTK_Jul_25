fec04eac0cc1584e8f93e4b33af0bb0252317e5e
dluffy121 <waghela.sonic@gmail.com>
2025-08-02 19:20:48 +0530

    - added Fan and Vacuum

---
+++ Assets/Scripts/World/Fan.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] float _pushStrength;
+
+    Player _playerObjRef;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        other.TryGetComponent(out _playerObjRef);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (_playerObjRef.gameObject != other.gameObject) return;
+
+        _playerObjRef.ApplyPushEffect(transform.forward, _pushStrength);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _playerObjRef.ApplyPullEffect(Vector3.zero, -1);
+        _playerObjRef = null;
+    }
+
+#if UNITY_EDITOR
+
+    BoxCollider _collider;
+
+    void OnDrawGizmos()
+    {
+        _collider ??= GetComponent<BoxCollider>();
+        Gizmos.color = Color.green;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(_collider.center, _collider.size);
+        Gizmos.DrawLine(_collider.center + Vector3.back, _collider.center + Vector3.forward);
+        Gizmos.DrawSphere(_collider.center + Vector3.forward, 0.1f);
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+
+#endif
+}
---
+++ Assets/Scripts/World/Vacuum.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour
+{
+    [SerializeField] float _pullStrength;
+
+    Player _playerObjRef;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        other.TryGetComponent(out _playerObjRef);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (_playerObjRef.gameObject != other.gameObject) return;
+
+        _playerObjRef.ApplyPullEffect(transform.position, _pullStrength);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _playerObjRef.ApplyPullEffect(transform.position, 0);
+        _playerObjRef = null;
+    }
+
+#if UNITY_EDITOR
+
+    CapsuleCollider _capsuleCollider;
+
+    void OnDrawGizmos()
+    {
+        _capsuleCollider ??= GetComponent<CapsuleCollider>();
+        Gizmos.color = Color.green;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireSphere(_capsuleCollider.center, _capsuleCollider.radius);
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+
+#endif
+}
