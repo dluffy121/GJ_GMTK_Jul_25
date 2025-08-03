4e9a421e42303fdae19255156a502b9457c4cee2
Manas Jagota <manasjagota5236@gmail.com>
2025-08-03 23:04:09 +0530

    Fixed issues in game

--- Assets/Scripts/Enemies/EnemyShooter.cs
+++ Assets/Scripts/Enemies/EnemyShooter.cs
@@ -24,8 +24,8 @@ public class EnemyShooter : MonoBehaviour
 
     private void OnPlayerDied()
     {
-        if(gameObject != null)
-            Destroy(gameObject);
+        //if(gameObject != null)
+        //    Destroy(gameObject);
     }
 
     private void Update()
