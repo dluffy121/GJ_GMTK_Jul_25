acd10d1d755f8088c75dcb5c1cd8c60695a94151
dluffy121 <waghela.sonic@gmail.com>
2025-08-03 17:33:36 +0530

    - more sound effects

--- Assets/Scripts/Enemies/InstaKillEnemy.cs
+++ Assets/Scripts/Enemies/InstaKillEnemy.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class InstaKillEnemy : EnemiesBase
 {
+    [SerializeField] AudioSource _as;
+    [SerializeField] AudioClip _hitSFX;
+    [SerializeField] float _hitSFXVol = 1;
+
     protected override void DamagePlayer()
     {
+        _as.PlayOneShot(_hitSFX, _hitSFXVol);
+
         Player.DecreasePlayerHealth(1);
         Destroy(gameObject);
     }
