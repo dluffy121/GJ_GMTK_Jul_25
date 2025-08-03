db84f14719d4a2f5351a253c3a2e60920e26fc81
Manas Jagota <manasjagota5236@gmail.com>
2025-08-03 16:46:22 +0530

    Updated UI

---
+++ Assets/Scripts/Systems/Move2DElement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move2DElement : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform rect;
+    [SerializeField]
+    Vector2 m_startPos;
+    [SerializeField]
+    Vector2 m_endPos;
+    [SerializeField]
+    float duration;
+    [SerializeField]
+    float m_waitForTimeBeforePlay;
+
+    private void Start()
+    {
+        StartCoroutine(MoveElement());
+    }
+
+    public IEnumerator MoveElement()
+    {
+        yield return new WaitForSeconds(m_waitForTimeBeforePlay);
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            rect.anchoredPosition = Vector2.Lerp(m_startPos, m_endPos, t);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        rect.anchoredPosition = m_endPos;
+        Destroy(gameObject);
+    }
+}
